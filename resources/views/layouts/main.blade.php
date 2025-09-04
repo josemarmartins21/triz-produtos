8aa43ade9f679425ffb5161fec7e9e243f7be576
Josemar Martins <josemar21@outlook.pt>
2025-09-04 20:02:30 +0100

    Terminei a página home, mas não TOTALMENTE

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Triz - produtos</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <header>
+        <h1><a href="#">Triz</a></h1>
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="#">Upload</a></li>
+                <li><a href="#">Dashboard</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        <h1>Triz Online</h1>
+        <p>&copy;Todos os direitos reservados a triz 2025</p>
+    </footer>
+</body>
+</html>
